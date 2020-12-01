963cd15c6055b8f7a4b2c3baf043872fc3195ad9
syedissam <45280974+syedissam@users.noreply.github.com>
2020-12-01 18:19:19 +0500

    Add files via upload

---
+++ src/test/java/com/simplecrud/springbootcrudexample/SpringBootCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecrud.springbootcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
